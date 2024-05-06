d5ceff713115e6bee350bc4ce3f7763f2403a08e
CHizzyEzu <155769930+CHizzyEzu@users.noreply.github.com>
2024-05-06 18:29:19 -0400

    Created Psuedo code for the program

---
+++ Psuedo Code.cpp
@@ -0,0 +1,31 @@
+/* Program is finding maximum and minimum element pairs 
+    The program then subtracts those pairs from each other 
+    and outputs the solution of the subtraction to terminal */
+
+ // Header Files:
+ // # include <iostream>
+ // #include <cmath>
+ // # include <limits>
+ // # include <climits>
+ // # include <cstdlib>
+ // using namespace std;
+
+ // -Global variable
+// -Array
+
+/* Created function that declares finds smallest and largest value in the array
+    then subtracts those values and returns the difference to function main */
+
+/* Created second function that find the pair of elements with the
+    smallest difference in value within the aray and subtracted them 
+    from each other larger number then smaller number 
+    Lastly I returned the difference to main*/
+
+/* Created a main function that had the sole purpose of outputing
+    the functions outside of main into terminal*/
+ 
+ 
+ 
+ 
+// Global Variable that initializes the size of the array
+
